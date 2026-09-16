ef2dc06b4d3093e7d068e4fe2842716e3ee08276
Crimtane <said-h95@hotmail.com>
2026-09-16 23:27:22 +0200

    Coded money formatting function

--- Testing/Crimtane/test_scene.gd
+++ Testing/Crimtane/test_scene.gd
@@ -4,6 +4,53 @@ extends Control
 const SHAPE: PackedScene = preload("uid://5qtpdede8o7j")
 const GRID_SLOT_BUTTON: PackedScene = preload("uid://dvinbarfymwhy")
 
+enum NotationStyle {
+	NONE,
+	ABBREVIATION,
+	SCIENTIFIC,
+	ENGINEERING
+}
+
+enum BigNumbers {
+	MILLION = 10**6,
+	BILLION = 10**9,
+	TRILLION = 10**12,
+	QUADRILLION = 10**15,
+	QUINTILLION = 10**18
+}
+
+enum BigNumbersScientific {
+	MILLION = 10**6,
+	TEN_MILLION = 10**7,
+	HUNDRED_MILLION = 10**8,
+	BILLION = 10**9,
+	TEN_BILLION = 10**10,
+	HUNDRED_BILLION = 10**11,
+	TRILLION = 10**12,
+	TEN_TRILLION = 10**13,
+	HUNDRED_TRILLION = 10**14,
+	QUADRILLION = 10**15,
+	TEN_QUADRILLION = 10**16,
+	HUNDRED_QUADRILLION = 10**17,
+	QUINTILLION = 10**18
+}
+
+var notations: Dictionary = {
+	MILLION = {ABBREVIATION = "M", SCIENTIFIC = "e6", ENGINEERING = "e6"},
+	TEN_MILLION = {ABBREVIATION = "M", SCIENTIFIC = "e7", ENGINEERING = "e6"},
+	HUNDRED_MILLION = {ABBREVIATION = "M", SCIENTIFIC = "e8", ENGINEERING = "e6"},
+	BILLION = {ABBREVIATION = "B", SCIENTIFIC = "e9", ENGINEERING = "e9"},
+	TEN_BILLION = {ABBREVIATION = "B", SCIENTIFIC = "e10", ENGINEERING = "e9"},
+	HUNDRED_BILLION = {ABBREVIATION = "B", SCIENTIFIC = "e11", ENGINEERING = "e9"},
+	TRILLION = {ABBREVIATION = "T", SCIENTIFIC = "e12", ENGINEERING = "e12"},
+	TEN_TRILLION = {ABBREVIATION = "T", SCIENTIFIC = "e13", ENGINEERING = "e12"},
+	HUNDRED_TRILLION = {ABBREVIATION = "T", SCIENTIFIC = "e14", ENGINEERING = "e12"},
+	QUADRILLION = {ABBREVIATION = "Qd", SCIENTIFIC = "e15", ENGINEERING = "e15"},
+	TEN_QUADRILLION = {ABBREVIATION = "Qd", SCIENTIFIC = "e16", ENGINEERING = "e15"},
+	HUNDRED_QUADRILLION = {ABBREVIATION = "Qd", SCIENTIFIC = "e17", ENGINEERING = "e15"},
+	QUINTILLION = {ABBREVIATION = "Qn", SCIENTIFIC = "e18", ENGINEERING = "e18"}
+}
+
 @export var gridSize: int = 3:
 	set(value):
 		gridSize = value
@@ -18,6 +65,7 @@ var gridStorage: Array[Dictionary]
 
 
 func _ready() -> void:
+	print(formatMoney(12345678, NotationStyle.SCIENTIFIC))
 	shapeButton.button_down.connect(_on_shape_button_press)
 	updateGrid()
 
@@ -134,3 +182,38 @@ func updateGrid() -> void:
 	
 	gridContainer.columns = gridSize
 	gridButtons = gridContainer.get_children()
+
+
+
+
+
+
+
+
+func formatMoney(value: int, notationStyle: NotationStyle) -> String:
+	if notationStyle == NotationStyle.NONE or value < BigNumbers.MILLION:
+		return str(value)
+	
+	var suffix: String = ""
+	var newValue: float = float(value)
+	
+	var testNum
+	
+	if notationStyle == NotationStyle.SCIENTIFIC:
+		for bigNum in BigNumbersScientific:
+			if value > BigNumbersScientific[bigNum]:
+				testNum = bigNum
+			else:
+				break
+	else:
+		for bigNum in BigNumbers:
+			if value > BigNumbers[bigNum]:
+				testNum = bigNum
+			else:
+				break
+	
+	suffix = notations[testNum][NotationStyle.find_key(notationStyle)]
+	newValue = float(value) / BigNumbersScientific[testNum]
+	
+	
+	return str(snapped(newValue, 0.001)) + suffix
