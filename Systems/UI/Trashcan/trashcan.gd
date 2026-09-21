b3fe58a8bf08e92f88e69ee672e557fcc860b444
Crimtane <said-h95@hotmail.com>
2026-09-21 22:11:26 +0200

    Trashcan gotta delete them all
    Clicking on the trashcan without something in hand now brings up a confirmation panel. Clicking yes will delete all shapes in the grid.

--- Systems/UI/Trashcan/trashcan.gd
+++ Systems/UI/Trashcan/trashcan.gd
@@ -2,15 +2,25 @@ extends Button
 
 @export var trashcanClosed: TextureRect
 @export var trashcanOpen: TextureRect
+@export var confirmationPanel: Panel
+@export var yesButton: Button
+@export var noButton: Button
 
 var mouseInside: bool = false
+var mouseInsideConfirmation = false
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	button_down.connect(clearShapeHeldByCursor)
 	button_up.connect(onTrashcanButtonUp)
 	mouse_entered.connect(onTrashcanMouseEntered)
 	mouse_exited.connect(OnTrashcanMouseExited)
+	yesButton.pressed.connect(deleteAllShapes)
+	noButton.pressed.connect(hideConfirmation)
+	focus_exited.connect(func(): if not mouseInsideConfirmation: hideConfirmation())
+	confirmationPanel.mouse_entered.connect(func(): mouseInsideConfirmation = true)
+	confirmationPanel.mouse_exited.connect(func(): mouseInsideConfirmation = false)
+
+
 
 func onTrashcanMouseEntered():
 	trashcanClosed.hide()
@@ -33,10 +43,16 @@ func onTrashcanButtonUp():
 
 func clearShapeHeldByCursor() -> void:
 	var shapeHeldByCursor = GameManager.UIManager.shapeHeldByCursor
+	var eraserHeldByCursor = GameManager.UIManager.eraserHeldByCursor
 	if shapeHeldByCursor:
 		GameManager.removeShapeFromStorage(shapeHeldByCursor)
 		shapeHeldByCursor.queue_free()
 		GameManager.calculateMoneyIncrement()
+		
+	elif not shapeHeldByCursor and not eraserHeldByCursor:
+		if GameManager.gridStorage and not confirmationPanel.visible:
+			confirmationPanel.visible = true
+		
 	
 	if mouseInside == true:
 		trashcanOpen.hide()
@@ -44,3 +60,13 @@ func clearShapeHeldByCursor() -> void:
 	else:
 		trashcanClosed.hide()
 		trashcanOpen.show()
+
+func deleteAllShapes() -> void:
+	confirmationPanel.visible = false
+	for dict in GameManager.gridStorage.duplicate():
+			GameManager.getShapeInStorageBySlot(dict["slot"]).queue_free()
+			GameManager.removeShapeFromStorageSlot(dict["slot"])
+			GameManager.calculateMoneyIncrement()
+
+func hideConfirmation() -> void:
+	confirmationPanel.visible = false
