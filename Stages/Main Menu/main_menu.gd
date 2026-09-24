308bbd2bdf617e778043b5be1c571643d07ead0e
Crimtane <said-h95@hotmail.com>
2026-09-24 21:10:51 +0200

    Gave main menu the ability to dance

--- Stages/Main Menu/main_menu.gd
+++ Stages/Main Menu/main_menu.gd
@@ -3,7 +3,13 @@ extends Control
 
 @export_file("*.tscn") var gamePath: String
 
-@onready var movingShapes = %MovingShapes
+@onready var parallaxLayer1 = %ParallaxLayer1
+@onready var parallaxLayer2 = %ParallaxLayer2
+@onready var parallaxLayer3 = %ParallaxLayer3
+@onready var shapes1 = parallaxLayer1.get_children()
+@onready var shapes2 = parallaxLayer2.get_children()
+@onready var shapes3 = parallaxLayer3.get_children()
+@onready var buttonPanelParallax = %ButtonPanelParallax
 @onready var startGameButton = %StartGame
 @onready var settingsButton = %Settings
 @onready var quitGameButton = %QuitGame
@@ -16,10 +22,23 @@ func _ready() -> void:
 	quitGameButton.pressed.connect(quitGame)
 
 func _process(_delta: float) -> void:
-	movingShapes.position = -get_local_mouse_position() * 0.02 + (get_viewport_rect().size / 2)
-	var shapes = movingShapes.get_children()
-	for shape in shapes:
-		if shapes.find(shape) > 0 and shapes.find(shape) < int(shapes.size()/2.0):
+	parallaxLayer1.position = -get_local_mouse_position() * 0.04 + (get_viewport_rect().size / 2)
+	parallaxLayer2.position = -get_local_mouse_position() * 0.02 + (get_viewport_rect().size / 2)
+	parallaxLayer3.position = -get_local_mouse_position() * 0.01 + (get_viewport_rect().size / 2)
+	buttonPanelParallax.position = -get_local_mouse_position() * 0.01 + (get_viewport_rect().size / 2) + Vector2(0.0, 40.0)
+	
+	for shape in shapes1:
+		if shapes1.find(shape) > 0 and shapes1.find(shape) < int(shapes1.size()/2.0):
+			shape.rotation += randf_range(0.0005, 0.002)
+		else:
+			shape.rotation -= randf_range(0.0005, 0.002)
+	for shape in shapes2:
+		if shapes2.find(shape) > 0 and shapes2.find(shape) < int(shapes2.size()/2.0):
+			shape.rotation += randf_range(0.0005, 0.002)
+		else:
+			shape.rotation -= randf_range(0.0005, 0.002)
+	for shape in shapes3:
+		if shapes3.find(shape) > 0 and shapes3.find(shape) < int(shapes3.size()/2.0):
 			shape.rotation += randf_range(0.0005, 0.002)
 		else:
 			shape.rotation -= randf_range(0.0005, 0.002)
